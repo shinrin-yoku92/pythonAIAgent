313837b5a75cdab54d605344416c6d93d6a47624
jfeast <jordan.feast@outlook.com>
2025-10-08 22:38:33 +0100

    Added CLI user prompt

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 
 def main():
     load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("Please provide a prompt as a command line argument.")
+        sys.exit(1)
+    
+    user_prompt = " ".join(args)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=user_prompt,
     )
 
     print(response.text)
