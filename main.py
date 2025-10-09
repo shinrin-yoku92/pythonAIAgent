77e7d925bbc1cb7c27f98578341fd7c8031e52b0
jfeast <jordan.feast@outlook.com>
2025-10-09 21:37:49 +0100

    --verbose flag added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 
@@ -13,20 +14,29 @@ def main():
     if not args:
         print("Please provide a prompt as a command line argument.")
         sys.exit(1)
-    
-    user_prompt = " ".join(args)
+
+    verbose = ("--verbose" in args)
+    prompt_args = [arg for arg in args if arg != "--verbose"]
+
+    user_prompt = " ".join(prompt_args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents=user_prompt,
+    contents=messages
     )
 
     print(response.text)
-    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("User prompt: ", user_prompt)
+        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
 
 
 
